43430c82ee28303f8f8c4d17a507710dc2ef17eb
zhangqibin <2308499325@qq.com>
2020-03-11 21:58:34 +0800

    c4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("bugfix");
+        System.out.println("zhanghjshjhdj");
     }
 }
